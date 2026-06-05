de355a303b267a2b794612b23658691e4f8563fa
ThanhDat <thanhdatvv02@gmail.com>
2026-06-05 11:18:33 +0700

    chore: remove unrelated modules, keep only parent pages

--- backend/src/main/java/com/beeacademy/backend/dto/request/OAuthSyncRequest.java
+++
@@ -1,6 +0,0 @@
-package com.beeacademy.backend.dto.request;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record OAuthSyncRequest(String fullName, String avatarUrl) {}
